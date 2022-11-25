eb966d3c68985f174ff8ecb1eb995a92f02d426d
JosephWamiti <91910681+Josephmugo8711@users.noreply.github.com>
2022-11-25 14:22:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ function upperCaseCustomerName(){
 function setBestCustomer(){
     bestCustomer = 'not bob';
 }
-
+//setBestCustomer(): Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'. 
+//(Poor Bob.) Also, poor us, declaring a global variable 
+//from inside a function is one of those things we would never want to do normally, but is good for us to explore right now.
 function overwriteBestCustomer(){
     bestCustomer = 'maybe bob';
 }
-
+//overwriteBestCustomer(): See the consequences of declaring a variable in global scope, 
+//by writing a new function called
+// overwriteBestCustomer() that changes that bestCustomer variable.
 function changeLeastFavoriteCustomer(){
     return 'Assignment to constant variable.'
 }
@@ -28,7 +32,7 @@ function changeLeastFavoriteCustomer(){
 //be sure to assign it some initial value. Finally, write a function called changeLeastFavoriteCustomer() 
 //that attempts to change that constant - notice what JavaScript does when you try to change the constant.
 
+const leastFavoriteCustomer = 'bob';
 function changeLeastFavoriteCustomer(){
-    const leastFavoriteCustomer = 'bob';
-    leastFavoriteCustomer = 'joseph';
+    leastFavoriteCustomer = 'joseph'
 }
